57a3c3f54c4c617ed487ed26933d6034d4e5d0ec
Ruslan Sorokin <strawberryladder@gmail.com>
2023-01-26 15:24:08 +0300

    refactor: move model into pkg folder

--- pkg/model/token.go
+++ pkg/model/token.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// Header of the both Access & Refresh tokens.
 type Header struct {
 	Algorithm string    `json:"alg"`
 	Type      string    `json:"typ"`
@@ -11,19 +12,23 @@ type Header struct {
 	ExpiredAt time.Time `json:"exp"`
 }
 
+// Payload of the Access Token.
 type Payload struct {
-	UserId userID
+	UserID string
 }
 
+// AccessToken used to store information for user's session.
 type AccessToken struct {
 	Header  Header
 	Payload Payload
 }
 
+// RefreshToken used to get a new token pair.
 type RefreshToken struct {
-	Header
+	Header Header
 }
 
+// TokenPair is a pair of both Access and Refresh tokens.
 type TokenPair struct {
 	AToken AccessToken
 	RToken RefreshToken
