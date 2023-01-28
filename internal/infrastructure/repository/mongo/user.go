486985b437373b56c5b5d96414d503ca2976e84d
Ruslan Sorokin <strawberryladder@gmail.com>
2023-01-28 15:02:20 +0300

    refactor(mongo): change value receivers to pointer receivers

--- internal/infrastructure/repository/mongo/user.go
+++ internal/infrastructure/repository/mongo/user.go
@@ -15,7 +15,7 @@ type UserRepository struct {
 	db *mongo.Collection
 }
 
-var _ repository.IUserRepository = UserRepository{}
+var _ repository.IUserRepository = (*UserRepository)(nil)
 
 // NewUserRepository is a default constructor
 func NewUserRepository(URI, dbName, collectionName string) *UserRepository {
@@ -25,7 +25,7 @@ func NewUserRepository(URI, dbName, collectionName string) *UserRepository {
 }
 
 // GetUserByID returns User model by UserID
-func (r UserRepository) GetUserByID(ctx context.Context, userID *string) (*model.User, error) {
+func (r *UserRepository) GetUserByID(ctx context.Context, userID *string) (*model.User, error) {
 	s := new(model.User)
 	res := r.db.FindOne(ctx, bson.M{
 		"id": userID,
@@ -38,7 +38,7 @@ func (r UserRepository) GetUserByID(ctx context.Context, userID *string) (*model
 }
 
 // GetUserByEmail returns User model by UserEmail
-func (r UserRepository) GetUserByEmail(ctx context.Context, userEmail *string) (*model.User, error) {
+func (r *UserRepository) GetUserByEmail(ctx context.Context, userEmail *string) (*model.User, error) {
 	s := new(model.User)
 	res := r.db.FindOne(ctx, bson.M{
 		"email": userEmail,
