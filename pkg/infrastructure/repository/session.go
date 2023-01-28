686aa2a5aabc1064acafd9c1bd7b19f8df8dace7
Ruslan Sorokin <strawberryladder@gmail.com>
2023-01-28 13:46:18 +0300

    refactor(repository): change interfaces

--- pkg/infrastructure/repository/session.go
+++ pkg/infrastructure/repository/session.go
@@ -6,8 +6,13 @@ import (
 	"github.com/ruslanSorokin/auth-service/pkg/model"
 )
 
-// ISessionRepoEngine is an interface with only custom CRUD-like operations for User entity.
-type ISessionRepoEngine interface {
-	DropUserRefreshTokenByUserID(ctx context.Context, userID int) (err error)
-	PatchUserRefreshTokenByUserID(ctx context.Context, userID int, rToken model.RefreshToken) (err error)
+// ISessionRepository is an interface with custom CRUD-like operations for Session entity.
+type ISessionRepository interface {
+	CreateSession(ctx context.Context, s *model.Session) (*string, error)
+	DeleteSessionByID(ctx context.Context, ID *string) error
+	DeleteSessionByUserID(ctx context.Context, userID *string) error
+	UpdateSessionByID(ctx context.Context, ID *string, s *model.Session) error
+	UpdateSessionByUserID(ctx context.Context, userID *string, s *model.Session) error
+	GetSessionByID(ctx context.Context, ID *string) (*model.Session, error)
+	GetSessionByUserID(ctx context.Context, userID *string) (*model.Session, error)
 }
