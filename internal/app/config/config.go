c942fc5b36aca1b427d3c64793ff662d0c55630e
Ruslan Sorokin <strawberryladder@gmail.com>
2023-02-01 15:31:08 +0300

    refactor(config): divide config into even smaller structs
    - Divide config into: Network, Service, JWT & Secret parts
    - Edit config template according to this change

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -7,32 +7,49 @@ import (
 )
 
 type (
-	app struct {
+	// Network part of the config contains all network stuff
+	Network struct {
 		MongoDBURI string `mapstructure:"MONGO_DB_URI"`
 
 		InternalServicePort int `mapstructure:"INTERNAL_SERVICE_PORT"`
 		ExternalServicePort int `mapstructure:"EXTERNAL_SERVICE_PORT"`
-
-		InternalPassword string `mapstructure:"INTERNAL_PASSWORD"`
 	}
 
-	jwt struct {
+	// Service part of the config contains everything used in usecases
+	Service struct {
+		MinUsernameLength int `mapstructure:"MIN_USERNAME_LENGTH"`
+		MinPasswordLength int `mapstructure:"MIN_PASSWORD_LENGTH"`
+
+		MaxUsernameLength int `mapstructure:"MAX_USERNAME_LENGTH"`
+		MaxPasswordLength int `mapstructure:"MAX_PASSWORD_LENGTH"`
+	}
+
+	// JWT contains everething for JWT configuration
+	JWT struct {
 		AccessTokenSecretKey string `mapstructure:"ACCESS_TOKEN_SECRET_KEY"`
 
 		AccessTokenExpiry  time.Duration `mapstructure:"ACCESS_TOKEN_EXPIRY"`
 		RefreshTokenExpiry time.Duration `mapstructure:"REFRESH_TOKEN_EXPIRY"`
 	}
+
+	// Secret part of the config contains the service's secrets
+	Secret struct {
+		InternalPassword string `mapstructure:"PASSWORD"`
+	}
 )
 
-// Config stores general parametrs for the entire authentication service
+// Config stores general parametrs for the authentication service
 type Config struct {
-	App app
-	JWT jwt
+	Network Network `mapstructure:",squash"`
+	Service Service `mapstructure:",squash"`
+	JWT     JWT     `mapstructure:",squash"`
+	Secret  Secret  `mapstructure:",squash"`
 }
 
 // Load config from path/name
 func Load(path, name string) (*Config, error) {
 	var cfg Config
+
 	viper.AddConfigPath(path)
 	viper.SetConfigName(name)
 
@@ -44,12 +61,7 @@ func Load(path, name string) (*Config, error) {
 		return nil, err
 	}
 
-	err = viper.Unmarshal(&cfg.App)
-	if err != nil {
-		return nil, err
-	}
-
-	err = viper.Unmarshal(&cfg.JWT)
+	err = viper.Unmarshal(&cfg)
 	if err != nil {
 		return nil, err
 	}
