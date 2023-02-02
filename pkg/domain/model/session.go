e7d7e528f01e3ab0751e692b7075e1f6d9cdc7c4
Ruslan Sorokin <strawberryladder@gmail.com>
2023-02-02 05:29:41 +0300

    refactor(model): change ExpiresIn Int field to ExpiresAt timestamp one

--- pkg/domain/model/session.go
+++ pkg/domain/model/session.go
@@ -1,11 +1,13 @@
 package model
 
+import "time"
+
 // Session stored in the authentication service's DataBase.
 type Session struct {
-	ID          string `bson:"id" json:"id"`
-	RToken      string `bson:"r_token" json:"r_token"`
-	UserID      string `bson:"user_id" json:"user_id"`
-	Fingerprint string `bson:"fingerprint" json:"fingerprint"`
-	IP          string `bson:"ip" json:"ip"`
-	ExpiresIn   int    `bson:"expires_in" json:"expires_in"`
+	ID          string    `bson:"id" json:"id"`
+	RToken      string    `bson:"r_token" json:"r_token"`
+	UserID      string    `bson:"user_id" json:"user_id"`
+	Fingerprint string    `bson:"fingerprint" json:"fingerprint"`
+	IP          string    `bson:"ip" json:"ip"`
+	ExpiresAt   time.Time `bson:"expires_at" json:"expires_at"`
 }
