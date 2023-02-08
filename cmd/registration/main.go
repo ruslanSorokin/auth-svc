bfb8c42bffc7787f1bdddbb5d799581804b35ad1
Ruslan Sorokin <strawberryladder@gmail.com>
2023-02-08 11:36:48 +0300

    feat(registration): add entry-point with config

---
+++ cmd/registration/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ruslanSorokin/auth-service/cmd/registration/config"
+)
+
+const configName = "default"
+const configPath = "cmd/registration/configs"
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	_, err := config.Load(configPath, configName)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
